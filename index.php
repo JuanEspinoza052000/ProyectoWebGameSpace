ecd5f3d0aeeb4b77f3b8238ac9a95b8199107c59
juane <juanantonioespinozasevilla@gmail.com>
2024-08-29 17:47:06 -0600

    Final

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                 <img src="images\juegos\elden-ring-plakat.jpg" alt="Producto 1">
                 <div class="overlay">
                     <h3>Elden Ring</h3>
-                    <h3>$50</h3>
+                    <h3>$26,000</h3>
                     <button class="add-to-cart">Comprar</button>
                 </div>
             </div>
@@ -24,7 +24,7 @@
                 <img src="images\juegos\1692738220-2079-jaquette-avant.webp" alt="Producto 1">
                 <div class="overlay">
                     <h3>Wukong</h3>
-                    <h3>$50</h3>
+                    <h3>$27,900</h3>
                     <button class="add-to-cart">Comprar</button>
                 </div>
             </div>
@@ -34,7 +34,7 @@
                 <img src="images\juegos\liesofp-1702510493314.jpg" alt="Producto 1">
                 <div class="overlay">
                     <h3>Lies of P</h3>
-                    <h3>$50</h3>
+                    <h3>$24,900</h3>
                     <button class="add-to-cart">Comprar</button>
                 </div>
             </div>
@@ -44,7 +44,7 @@
                 <img src="images\juegos\Minecraft_cover.webp" alt="Producto 1">
                 <div class="overlay">
                     <h3>Minecraft</h3>
-                    <h3>$50</h3>
+                    <h3>$14,900</h3>
                     <button class="add-to-cart">Comprar</button>
                 </div>
             </div>
@@ -54,7 +54,7 @@
                 <img src="images\juegos\Ghost_Recon_Wildlands_cover_art.jpg" alt="Producto 1">
                 <div class="overlay">
                     <h3>Wildlands</h3>
-                    <h3>$50</h3>
+                    <h3>$26,900</h3>
                     <button class="add-to-cart">Comprar</button>
                 </div>
             </div>
@@ -64,7 +64,7 @@
                 <img src="images\juegos\Terraria_Steam_artwork.jpg" alt="Producto 1">
                 <div class="overlay">
                     <h3>Terraria</h3>
-                    <h3>$5</h3>
+                    <h3>$5000</h3>
                     <button class="add-to-cart">Comprar</button>
                 </div>
             </div>
@@ -81,93 +81,114 @@
 
 
         <div class="card-container">
-            <div class="card">
-                <img src="images\juegos\elden-ring-plakat.jpg" alt="Producto 1">
+           
+        <div class="card" data-id="7" data-price="5">
+                <img src="images\juegos\8d25153bec94068872e2a5ff021e5ff7.png" alt="Producto 1">
                 <div class="overlay">
-                    <h3>Producto 1</h3>
-                    <h3>Precio</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\1692738220-2079-jaquette-avant.webp" alt="Producto 2">
-                <div class="overlay">
-                    <h3>Producto 2</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\liesofp-1702510493314.jpg" alt="Producto 3">
-                <div class="overlay">
-                    <h3>Producto 3</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\Ghost_Recon_Wildlands_cover_art.jpg" alt="Producto 4">
-                <div class="overlay">
-                    <h3>Producto 4</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\Minecraft_cover.webp" alt="Producto 5">
-                <div class="overlay">
-                    <h3>Producto 5</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\Terraria_Steam_artwork.jpg" alt="Producto 6">
-                <div class="overlay">
-                    <h3>Terraria</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\elden-ring-plakat.jpg" alt="Producto 1">
-                <div class="overlay">
-                    <h3>Producto 1</h3>
-                    <h3>Precio</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\1692738220-2079-jaquette-avant.webp" alt="Producto 2">
-                <div class="overlay">
-                    <h3>Producto 2</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\liesofp-1702510493314.jpg" alt="Producto 3">
-                <div class="overlay">
-                    <h3>Producto 3</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\Ghost_Recon_Wildlands_cover_art.jpg" alt="Producto 4">
-                <div class="overlay">
-                    <h3>Producto 4</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\Minecraft_cover.webp" alt="Producto 5">
-                <div class="overlay">
-                    <h3>Producto 5</h3>
-                    <button>Comprar</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images\juegos\Terraria_Steam_artwork.jpg" alt="Producto 6">
-                <div class="overlay">
-                    <h3>Terraria</h3>
-                    <button>Comprar</button>
+                    <h3>SCP Secret Lab</h3>
+                    <h3>$0</h3>
+                    <button class="add-to-cart">Comprar</button>
                 </div>
             </div>
 
+            <div class="card" data-id="8" data-price="5">
+                <img src="images\juegos\enihR6QwSYiWCNl2HdPfV6R6.jpg" alt="Producto 1">
+                <div class="overlay">
+                    <h3>Rust</h3>
+                    <h3>$15,900</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="9" data-price="5">
+                <img src="images\juegos\capsule_616x353.jpg" alt="Producto 1">
+                <div class="overlay">
+                    <h3>Forza Horizon 5</h3>
+                    <h3>$34,000</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="10" data-price="5">
+                <img src="images\juegos\eurogamer-zjp1vx.webp" alt="Producto 1">
+                <div class="overlay">
+                    <h3>GTA V</h3>
+                    <h3>$4915</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="11" data-price="5">
+                <img src="images\juegos\FkzwjnJknkrFlozkTdeQBMub.jpg" alt="Producto 1">
+                <div class="overlay">
+                    <h3>Resident Evil Village</h3>
+                    <h3>$25,400</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="12" data-price="5">
+                <img src="images\juegos\Hollow_Knight_first_cover_art.webp" alt="Producto 1">
+                <div class="overlay">
+                    <h3>Hollow knight</h3>
+                    <h3>$6500</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="13" data-price="5">
+                <img src="images\juegos\m7gCTrMRQBTPHi87Mi3leVVY.webp" alt="Producto 1">
+                <div class="overlay">
+                    <h3>Little Nightmares II </h3>
+                    <h3>$14,250</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="14" data-price="5">
+                <img src="images\juegos\nsfh-hero-large-keyart-16x9-xl.jpg.adapt.crop1x1.320w.jpg" alt="Producto 1">
+                <div class="overlay">
+                    <h3>Need for Speed</h3>
+                    <h3>$40,000</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="15" data-price="5">
+                <img src="images\juegos\resident-evil-4-gold-edition-digital-900w.png" alt="Producto 1">
+                <div class="overlay">
+                    <h3>Resident Evil 4</h3>
+                    <h3>$22,300</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="16" data-price="5">
+                <img src="images\juegos\tablet-1.jpg" alt="Producto 1">
+                <div class="overlay">
+                    <h3>Zelda Breath of the Wild</h3>
+                    <h3>$27,300</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="17" data-price="5">
+                <img src="images\juegos\dragon_ball_z_kakarot.jpg" alt="Producto 1">
+                <div class="overlay">
+                    <h3>Dragon Ball z Kakarot</h3>
+                    <h3>$5</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
+
+            <div class="card" data-id="18" data-price="5">
+                <img src="images\juegos\718BSrjQjcL._AC_UF1000,1000_QL80_.jpg" alt="Producto 1">
+                <div class="overlay">
+                    <h3>FC-24</h3>
+                    <h3>$5</h3>
+                    <button class="add-to-cart">Comprar</button>
+                </div>
+            </div>
 
 
 
